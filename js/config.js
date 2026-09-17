b91748a29463ca4d544698d69d470939364e403c
HankLumbard <158756999+HankLumbard@users.noreply.github.com>
2026-09-17 19:45:08 -0400

    Update test API deployment URL

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // FILL THIS IN once you've deployed the Apps Script as a Web App
 // (Deploy > New deployment > Web app). Paste the URL that ends in /exec.
 // ------------------------------------------------------------------
-const SHEET_API_URL = "https://script.google.com/macros/s/AKfycby_XdTGvYffj4tv4HiKhHM_D3ggnHCO8R4aruvTVdrLETMnBpKzYIlM89YH/exec";
+const SHEET_API_URL = "https://script.google.com/macros/s/AKfycby_XdTGvYffj4tv4na99u-i4HiKhHM_D3ggnHCO8R4aruvTVdrLETMnBpKzYIlM89YH/exec";
 
 // Season setup mode:
 //   "live"   = Settings/Castaways are read from Google Sheets during setup
